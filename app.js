90a9d7d4b1ed4cce22988d9ffe47fa3b72b7736c
Kevin Leung <keleung@microsoft.com>
2016-10-18 23:58:33 -0500

    app.js

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var connector = new builder.ChatConnector({
     appId: process.env.APP_ID,
     appPassword: process.env.APP_PASS
 });
-console.log('%s listening to %s', process.env.APP_ID, process.env.APP_PASS);
 
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
